4fa77f2cf938fdbfae9adea6761fb5905d897d27
Gibo Kim <gibo9809@gmail.com>
2025-11-10 23:43:15 +0900

    [종만북|동적 계획법]: 8.2 와일드카드-부분문제를 활용한 동적계획법 풀이 방법

--- src/coding_test/종만북/동적계획법/와일드카드_dp.java
+++ src/coding_test/종만북/동적계획법/와일드카드_dp.java
@@ -27,4 +27,44 @@ public class 와일드카드_dp {
 
         return false;
     }
+
+    // -1 : 계산되지 않는다, 1 : 해당 입력들이 서로 대응된다, 0 : 해당 입력들이 서로 대응되지 않는다.
+    private int[][] cache;
+    // 패턴과 문자열
+    String W, S;
+
+    // 와일드카드 패턴 W[w...]가 문자열 S[s...]에 대응되는지 여부를 반환한다.
+    private boolean matchMemoized(int w, int s) {
+        if (cache[w][s] != -1) {
+            return cache[w][s] == 1;
+        }
+
+        // W[w]와 S[s]를 맞춰 나간다.
+        while (s < S.length() && w < W.length() &&
+                (W.charAt(w) == '?' || W.charAt(w) == S.charAt(s))) {
+            w++;
+            s++;
+        }
+
+        // 더 이상 대응할 수 없으면 왜 while문이 끝났는지 확인한다.
+        // 패턴 끝에 도달한 경우에는 문자열도 끝났어야 대응된다.
+        if (w == W.length()) {
+            cache[w][s] = (s == S.length()) ? 1 : 0;
+            return cache[w][s] == 1;
+        }
+
+        // *를 만나서 끝난경우에는 *에 몇 글자를 대응해야 할 지 재귀호출하며 확인한다.
+        if (W.charAt(w) == '*') {
+            for (int skip = 0; s + skip <= S.length(); skip++) {
+                if (matchMemoized(w+1, s + skip)) {
+                    cache[w][s] = 1;
+                    return true;
+                }
+            }
+        }
+
+        // 이 외의 모든 경우에는 false를 반환한다.
+        cache[w][s] = 0;
+        return false;
+    }
 }
