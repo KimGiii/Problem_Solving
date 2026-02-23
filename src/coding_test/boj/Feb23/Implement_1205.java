c4225eb902ec71f5408752d6df18e6e30bb81d6e
KimGiii <gibo9809@gmail.com>
2026-02-24 00:18:01 +0900

    [BOJ] 1205:등수 구하기|Implement

---
+++ src/coding_test/boj/Feb23/Implement_1205.java
@@ -0,0 +1,46 @@
+package coding_test.boj.Feb23;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Implement_1205 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int taesooScore = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+
+        if (N == 0) {
+            System.out.println(1);
+            return;
+        }
+
+        int[] scores = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            scores[i] = Integer.parseInt(st.nextToken());
+        }
+
+        if (N == P && taesooScore <= scores[N - 1]) {
+            System.out.println(-1);
+            return;
+        }
+
+        int taesooRank = 1;
+        for (int i = 0; i < N; i++) {
+            if (taesooScore < scores[i]) {
+                taesooRank++;
+            } else {
+                break;
+            }
+        }
+
+        System.out.println(taesooRank);
+    }
+}
