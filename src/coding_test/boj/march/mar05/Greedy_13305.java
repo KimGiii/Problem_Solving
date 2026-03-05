81beb4f238f6fe7eb2c8c2ef1c2f275dce2f3bca
KimGiii <gibo9809@gmail.com>
2026-03-05 23:59:58 +0900

    [BOJ] 13305: 주유소

--- src/coding_test/boj/march/mar05/Greedy_13305.java
+++ src/coding_test/boj/march/mar05/Greedy_13305.java
@@ -8,28 +8,33 @@ public class Greedy_13305 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         //도시의 개수
         int citiesCount = Integer.parseInt(br.readLine());
-
         // 두 도시 사이 거리
+        long[] distances = new long[citiesCount - 1];
+        // 도시별 리터 당 가격
+        long[] prices = new long[citiesCount];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] distances = new int[citiesCount - 1];
-        int totalDistance = 0;
         for (int i = 0; i < distances.length; i++) {
             distances[i] = Integer.parseInt(st.nextToken());
-            totalDistance += distances[i];
         }
 
-        // 도시별 리터 당 가격
         st = new StringTokenizer(br.readLine());
-        int[] prices = new int[citiesCount];
         for (int i = 0; i < citiesCount; i++) {
             prices[i] = Integer.parseInt(st.nextToken());
         }
 
-        int minCost = distances[0] * prices[0];
-        for (int i = 1; i < distances.length; i++) {
-            minCost += Math.min(distances[i] * prices[i], distances[i] * prices[i-1]);
+        long totalPrice = 0;
+        long minPrice = prices[0];
+
+        for (int i = 0; i < distances.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+
+            totalPrice += distances[i] * minPrice;
         }
 
-        System.out.println(minCost);
+        System.out.println(totalPrice);
+
     }
 }
