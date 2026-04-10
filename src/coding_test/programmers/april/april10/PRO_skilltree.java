305bba728b265a11525b16adee7c0dc1229ea627
KimGiii <gibo9809@gmail.com>
2026-04-10 23:36:39 +0900

    [PROGRAMMERS] 스킬트리 | Implementation

---
+++ src/coding_test/programmers/april/april10/PRO_skilltree.java
@@ -0,0 +1,24 @@
+package coding_test.programmers.april.april10;
+
+public class PRO_skilltree {
+    class Solution {
+        public int solution(String skill, String[] skill_trees) {
+            int answer = 0;
+
+            for (String tree : skill_trees) {
+                StringBuilder sb = new StringBuilder();
+                for (char c : tree.toCharArray()) {
+                    if (skill.indexOf(c) != -1) {
+                        sb.append(c);
+                    }
+                }
+
+                if (skill.startsWith(sb.toString())) {
+                    answer++;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
